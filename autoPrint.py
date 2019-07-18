b4b8c58bad5f9a0efd11ea5eaf64a22405c45393
Administrator <405886056@qq.com>
2019-07-19 00:11:38 +0800

    add

---
+++ autoPrint.py
@@ -0,0 +1,30 @@
+#python批量打印word文件
+#pip install pypiwin32
+import tempfile
+import win32api
+import win32print
+import os
+
+def printer_loading(filename):
+    open(filename, "r")
+    win32api.ShellExecute(
+        0,
+        "print",
+        filename,
+        #
+        # If this is None, the default printer will
+        # be used anyway.
+        #
+        '/d:"%s"' % win32print.GetDefaultPrinter(),
+        ".",
+        0
+    )
+
+path=r'I:\PycharmProjects\aboutWorkGit\ok'
+for a, b, c in os.walk(path):
+    print(c)
+for i in c:
+    f = os.path.join(path, i)
+    if f.endswith("docx"):
+        print(f)
+        printer_loading(f)
--- renameFile.py
+++ renameFile.py
@@ -11,6 +11,7 @@ file_list = os.listdir(file_dir)
 for file_name in file_list:
     #name_reg = re.compile(r'(^\[电影天堂www.dy2018.com\]|^\[阳光电影www.ygdy8.net\].|^阳光电影www.ygdy8.net|^\[阳光电影www.ygdy8.com\].'
     #                      r'|^阳光电影www.ygdy8.com.)')
+    #拼接  正则表达式
     name_reg = re.compile(r'(\[电影天堂www.dy2018.com\]|\[最新电影www.66ys.tv\]|.电影天堂.www.dy2018.com|\[飘花www.piaohua.com\])')
     name_replace = name_reg.sub('', file_name)
     #print(file_name)
